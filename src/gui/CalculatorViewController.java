1a343d356c9af59e14d71cabba9244a5b8c70955
Marcos Henrique <marcosbilobram@hotmail.com>
2022-02-08 19:11:17 -0300

    Implementing CalculatorViewController

---
+++ src/gui/CalculatorViewController.java
@@ -0,0 +1,206 @@
+package gui;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class CalculatorViewController implements Initializable{
+	
+	@FXML
+	private TextField TextFieldPanel; 
+	
+	@FXML
+	private Button buttonEqual;
+	
+	@FXML
+	private Button buttonDelete;
+	
+	@FXML
+	private Button buttonClear;
+	
+	@FXML
+	private Button buttonPercentage;
+	
+	@FXML
+	private Button buttonOneDividedfor;
+	
+	@FXML
+	private Button buttonSquared;
+	
+	@FXML
+	private Button buttonSquareRoot;
+	
+	@FXML
+	private Button buttonDivision;
+	
+	@FXML
+	private Button buttonMultiplication;
+	
+	@FXML
+	private Button buttonMinus;
+	
+	@FXML
+	private Button buttonPlus;
+	
+	@FXML
+	private Button buttonComma;
+	
+	@FXML
+	private Button buttonChangeSignal;
+	
+	@FXML
+	private Button buttonZero;
+	
+	@FXML
+	private Button buttonOne;
+	
+	@FXML
+	private Button buttonTwo;
+	
+	@FXML
+	private Button buttonThree;
+	
+	@FXML
+	private Button buttonFour;
+	
+	@FXML
+	private Button buttonFive;
+	
+	@FXML
+	private Button buttonSix;
+	
+	@FXML
+	private Button buttonSeven;
+	
+	@FXML
+	private Button buttonEight;
+	
+	@FXML
+	private Button buttonNine;
+	
+	@FXML
+	public void onButtonEqualAction() {
+		//Int result = currentOperation(N1, CurrentSignal, N2);
+		
+	}
+	
+	@FXML
+	public void onButtonDeleteAction() {
+		
+	}
+	
+	@FXML
+	public void onButtonClearAction() {
+		
+	}
+	
+	@FXML
+	public void onButtonPercentageAction() {
+		
+	}
+	
+	@FXML
+	public void onButtonOneDivideForAction() {
+		
+	}
+	
+	@FXML
+	public void onButtonSquaredAction() {
+		
+	}
+	
+	@FXML
+	public void onButtonSquaredRootAction() {
+		
+	}
+	
+	@FXML
+	public void onButtonDivisionAction() {
+		
+	}
+	
+	@FXML
+	public void onButtonMultiplicationAction() {
+		
+	}
+	
+	@FXML
+	public void onButtonMinusAction() {
+		
+	}
+	
+	@FXML
+	public void onButtonPlusAction() {
+		
+	}
+	
+	@FXML
+	public void onButtonCommaAction() {
+		
+	}
+	
+	@FXML
+	public void onButtonChangeSignalAction() {
+		
+	}
+	
+	@FXML
+	public void onButtonOneAction() {
+		
+	}
+	
+	@FXML
+	public void onButtonTwoAction() {
+		
+	}
+	
+	@FXML
+	public void onButtonThreeAction() {
+		
+	}
+	
+	@FXML
+	public void onButtonFourAction() {
+		
+	}
+	
+	@FXML
+	public void onButtonFiveAction() {
+		
+	}
+	
+	@FXML
+	public void onButtonSixAction() {
+		
+	}
+	
+	@FXML
+	public void onButtonSevenAction() {
+		
+	}
+	
+	@FXML
+	public void onButtonEightAction() {
+		
+	}
+	
+	@FXML
+	public void onButtonNineAction() {
+		
+	}
+	
+	@FXML
+	public void onButtonZeroAction() {
+		
+	}
+	
+
+	@Override
+	public void initialize(URL url, ResourceBundle rb) {
+	}
+
+}
